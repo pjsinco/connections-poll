0e8d9cc287b572b4f233e7b30515103e46bf048b
Patrick Sinco <psinco@gmail.com>
2013-10-15 14:46:29 -0500

    removes close-window button because it doesn't work in ffox

--- already-voted.php
+++ already-voted.php
@@ -1,6 +1,5 @@
 <?php require('poll-template.php'); ?>
     <h3>You've already voted&mdash;thank you!</h3> 
-    <button type='button' onclick="window.open('', '_self', ''); window.close();">Close window</button>
   </div>
   </div>
 </body>
--- thank-you.php
+++ thank-you.php
@@ -1,6 +1,5 @@
 <?php require('poll-template.php'); ?>
     <h3>Thank you for voting!</h3> 
-    <button type='button' onclick="window.open('', '_self', ''); window.close();">Close window</button>
   </div>
   </div>
 </body>
