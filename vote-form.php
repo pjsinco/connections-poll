5362bd92d25baa4c56173a9ee2308b5229c976fd
Patrick Sinco <psinco@gmail.com>
2013-10-14 19:08:11 -0500

    pulling info from db ok

--- vote-form.php
+++ vote-form.php
@@ -2,18 +2,20 @@
 
 /* display a vote form */
 require_once('vote-config.php');
-$poll = $_GET('poll');
-if (!is_numeric($poll)) {
-  die('Invalid poll');
-}
+$poll = $_GET['poll'];
+  if (!is_numeric($poll)) {
+    die('Invalid poll');
+  }
+
 
 /* look up the poll in the database */
 $sql = "SELECT p.question, a.answer, a.answer_id
-        FROM conn_poll p inner join conn_answer a
-        ON p.id = a.poll_id
-        p.id = $poll;";
+        FROM conn_poll p INNER JOIN conn_answer a
+          ON p.id = a.poll_id
+        WHERE p.id = $poll";
 
-$result = mysqli_query($sql, $db) or die("mysql error: " . mysql_error());
+
+$result = mysql_query($sql, $db) or die("mysql error: " . mysql_error());
 
 if (mysql_num_rows($result) == 0) {
   die('Invalid poll.');
@@ -28,8 +30,26 @@ if ($_COOKIE["poll_voted $poll"]) {
 /* vote form */
 $question_list = "";
 while($row = mysql_fetch_array($result)) {
-
+  $question = $row['question'];
+  $question_list .= '<li><input name="answer" type="radio" value="' . $row['answer_id'] . '"> ' . $row['answer'] . '</li>';
 }
 
-
 ?>
+<!DOCTYPE html>
+<html>
+<head>
+
+</head>
+<body>
+<p>Q: <?php echo $question; ?></p>
+<form action="vote_process.php" method="post">
+<ul>
+<?php echo $question_list; ?>
+</ul>
+<input name='poll' type='hidden' value='<?php echo $poll; ?>'>
+<input name='' type='submit' value='Vote'>
+</form>
+
+
+</body>
+</html>
